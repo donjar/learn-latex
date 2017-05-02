b0b7ac87bf038a45c22253a063e46f6f70388842
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-05-02 10:34:01 +0800

    react-katex added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { InlineMath, BlockMath } from 'react-katex';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="container">
+          <h1>Belajar LaTeX Dalam 10 Menit</h1>
+          <InlineMath math="\frac{1}{2\pi i} \int_\gamma \frac{f(z)}{z - w}\, dw = f(w)" />
+          <BlockMath math="\frac{1}{2\pi i} \int_\gamma \frac{f(z)}{z - w}\, dw = f(w)" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'katex/dist/katex.min.css';
 
 ReactDOM.render(
   <App />,
